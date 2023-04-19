09535a704d1043eba578c6ef968586a2faeec3b6
Josh Iden <iden.josh@gmail.com>
2023-04-19 19:38:19 -0400

    Update 01answer.js

--- module6/d3_lab/01answer.js
+++ module6/d3_lab/01answer.js
@@ -1,10 +1,10 @@
 d3.csv('ue_industry.csv', data => {
 
-    const xScale = d3.scaleLinear()
+    const xScalea = d3.scaleLinear()
         .domain(d3.extent(data, d => +d.index))
         .range([20, 1180]);
     
-    const yScale = d3.scaleLinear()
+    const yScalea = d3.scaleLinear()
         .domain(d3.extent(data, d => +d.Agriculture))
         .range([580, 20]);
     
