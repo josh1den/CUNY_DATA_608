9317a61be7869b00dd1390ad2fdb9e8d128ad391
Josh Iden <66791104+josh1den@users.noreply.github.com>
2023-04-19 19:25:34 -0400

    Update 01answer.js

--- module6/d3_lab/01answer.js
+++ module6/d3_lab/01answer.js
@@ -1,8 +1,21 @@
 d3.csv('ue_industry.csv', data => {
 
     // Define your scales and generator here.
+    const xScale = d3.scaleLinear()
+        .domain(d3.extent(data, d => +d.index))
+        .range([20, 1180]);
+    
+    const yScale = d3.scaleLinear()
+        .domain(d3.extent(data, d => +d.Agriculture))
+        .range([580, 20]);
+    
+    let linea = d3.line()
+        .x(d => xScalea(d.index))
+        .y(d => yScalea(+d.Agriculture));
 
-    d3.select('#answer1')
-        // append more elements here
+    d3.select('#part3')
+        .append('path')
+        .attr('d', linea(data))
+        .attr('stroke', '#2e2928')
 
 });
